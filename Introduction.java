d9ef3773107351392b2fc74ce4974c359561dbe8
GarethW1994 <williams.gareth0@gmail.com>
2017-08-25 12:19:02 +0200

    initial commit

---
+++ Introduction.java
@@ -0,0 +1,10 @@
+import java.io.Console;
+
+public class Introduction {
+  public static void main(String[] args) {
+    Console console = System.console();
+
+    //print out welcome to Introduction test
+    console.printf("Welcome to Java Introduction");
+  }
+}
